62dda5a2a41a8690d717b8df5bebeda043f763fd
CecilyK <cecily.kingori@strathmore.edu>
2020-07-15 03:21:13 +0300

    Lab 5 Update

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    //
+    public function allcars(){
+        //read all cars
+    }
+
+    public function particularcar($id){
+        //read particluar car read by id
+    }
+
+    public function newcar(){
+        //create a new car
+    }
 }
